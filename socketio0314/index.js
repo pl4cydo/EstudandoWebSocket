0a12dccbaeca8d298d1981811c44d33cd513e236
pl4cydo <psl2@discente.ifpe.edu.br>
2023-03-16 09:11:13 -0300

    chat basico finalizado

--- socketio0314/index.js
+++ socketio0314/index.js
@@ -22,6 +22,12 @@ io.on('connection', (socket) => {
     });
 });
 
+io.on('connection', (socket) => {
+    socket.on('chat message', (msg) =>{
+        io.emit('chat message', msg);
+    })
+})
+
 server.listen(3000, () => {
     console.log('listening on *: 3000');
 });
